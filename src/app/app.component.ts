c82b70e2f9ff237be65d6b8f032e20898c65e016
Qwaz <qwazpia@gmail.com>
2017-02-21 23:43:46 +0900

    Implement file drag & drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,68 +2,50 @@ import {Component} from "@angular/core";
 declare let Materialize: Materialize.Materialize;
 
 export class File {
-    fileName: String;
+    name: string;
+    path: string;
+    size: number;
+    type: string;
 }
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: `
-<div class="flex-v-container" id="main-window">
-    <nav class="flex-none">
-        <div class="nav-wrapper">
-            <span class="brand-logo center">Automatan</span>
-        </div>
-    </nav>
-    <div class="flex1 flex-h-container">
-        <div class="flex2">
-            <table class="highlight">
-                <thead>
-                    <tr><th data-field="fileName">Files</th></tr>
-                </thead>
-                <tbody>
-                    <tr *ngFor="let file of files"><td>{{file.fileName}}</td></tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="flex1 border-left flex-v-container inner-padding">
-            <div class="flex1">
-                <form>
-                    <div class="input-field">
-                        <input id="raws_info" placeholder="(not detected)" type="text" class="validate">
-                        <label for="raws_info">Raws</label>
-                    </div>
-                    <div class="input-field">
-                        <input id="title_info" value="Gochuumon wa Usagi Desuka 2" type="text" class="validate">
-                        <label for="title_info">Title</label>
-                    </div>
-                    <div class="input-field">
-                        <input id="episode_style_info" value=" - %" type="text" class="validate">
-                        <label for="episode_style_info">Episode</label>
-                    </div>
-                    <div class="input-field">
-                        <input id="codec_info" value="(BD 1280x720 x264 AAC)" type="text" class="validate">
-                        <label for="codec_info">Codec</label>
-                    </div>
-                </form>
-            </div>
-            <div class="flex-none" id="actions">
-                <a class="btn-floating btn-large waves-effect waves-light right"><i class="material-icons">done</i></a>
-            </div>
-        </div>
-    </div>
-</div>
-`
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent {
-    files: File[] = [
-        { fileName: 'Gochuumon wa Usagi Desuka 2 - 01 (BD 1280x720 x264 AAC)' },
-        { fileName: 'Gochuumon wa Usagi Desuka 2 - 02 (BD 1280x720 x264 AAC)' },
-        { fileName: 'Gochuumon wa Usagi Desuka 2 - 03 (BD 1280x720 x264 AAC)' },
-        { fileName: 'Gochuumon wa Usagi Desuka 2 - 04 (BD 1280x720 x264 AAC)' },
-    ];
+    files: File[] = [];
 
     ngAfterViewInit() {
         Materialize.updateTextFields();
+    }
+
+    preventDefault() {
+        return false;
+    }
+
+    onDropFiles(e: DragEvent) {
+        console.log(e);
+
+        for (let i = 0; i < e.dataTransfer.files.length; i++) {
+            let f = e.dataTransfer.files[i];
+            console.log(f);
+            this.files.push({
+                name: f.name,
+                path: f.path,
+                size: f.size,
+                type: f.type
+            });
+        }
+        this.files.sort((f1, f2) => f1.name.localeCompare(f2.name));
+
+        console.log(this.files);
+
+        return false;
+    }
+
+    clearFiles() {
+        this.files = [];
     }
 }
