d8ad3c1176931bf26451ee93caf25966e205e847
Qwaz <qwazpia@gmail.com>
2017-03-29 17:40:56 +0900

    Implement automatic pattern detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {AfterViewChecked, Component} from "@angular/core";
 
 declare let Materialize: Materialize.Materialize;
 
@@ -14,11 +14,13 @@ export enum PatternException {
     Various
 }
 
+type PatternValue = PatternException | string;
+
 export interface Pattern {
-    raws: PatternException | string;
-    title: PatternException | string;
-    episode: PatternException | string;
-    codec: PatternException | string;
+    raws: PatternValue;
+    title: PatternValue;
+    episode: PatternValue;
+    codec: PatternValue;
 }
 
 @Component({
@@ -26,18 +28,18 @@ export interface Pattern {
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
     files: File[] = [];
 
-    exceptionString(pattern: PatternException | string) {
-        if (pattern === PatternException.Empty) return "(not detected)";
-        else if (pattern === PatternException.Various) return "(various)";
+    exceptionString(patternValue: PatternValue) {
+        if (patternValue === PatternException.Empty) return "(not detected)";
+        else if (patternValue === PatternException.Various) return "(various)";
         else return "";
     }
 
-    patternString(pattern: PatternException | string) {
-        if (typeof pattern === "string") {
-            return pattern;
+    patternString(patternValue: PatternValue) {
+        if (typeof patternValue === "string") {
+            return patternValue;
         } else {
             return "";
         }
@@ -45,9 +47,9 @@ export class AppComponent {
 
     initialPattern: Pattern = {
         raws: PatternException.Empty,
-        title: "Gochuumon wa Usagi Desuka 2",
-        episode: PatternException.Various,
-        codec: "(BD 1280x720 x264 AAC)",
+        title: PatternException.Empty,
+        episode: PatternException.Empty,
+        codec: PatternException.Empty,
     };
 
     targetPattern: Pattern;
@@ -57,30 +59,93 @@ export class AppComponent {
     }
 
     onDropFiles(e: DragEvent) {
+        function updatePattern(pattern: Pattern, key: keyof Pattern, value: string): void {
+            if (typeof pattern[key] == 'string') {
+                if (pattern[key] != value) {
+                    pattern[key] = PatternException.Various;
+                }
+            } else {
+                if (pattern[key] == PatternException.Empty) {
+                    pattern[key] = value;
+                }
+            }
+        }
+
+        function episodePatternExtract(episodePattern: string, episodeNumber: string): string {
+            let lastIndex = episodePattern.lastIndexOf(episodeNumber);
+            let len = episodeNumber.length;
+
+            return episodePattern.substring(0, lastIndex) +
+                    '?' + episodePattern.substring(lastIndex+len);
+        }
+
+        let pattern: RegExp = /^((?:\[.*\])?)\s*(.*?(?:[_\s]S?\d+)?)\s+((?:[\-#제第E]|Ep\.?|OP|ED|Vol\.?)?\s*(\d+(?:\.\d)?)(?:[화話]|s|ns)?)(.*?)\s*(\([^()]{5,}\))?\..{2,4}$/i;
+
         console.log(e);
+
+        this.initialPattern.raws = PatternException.Empty;
+        this.initialPattern.title = PatternException.Empty;
+        this.initialPattern.episode = PatternException.Empty;
+        this.initialPattern.codec = PatternException.Empty;
 
         for (let i = 0; i < e.dataTransfer.files.length; i++) {
             let f = e.dataTransfer.files[i];
             console.log(f);
-            this.files.push({
-                name: f.name,
-                path: f.path,
-                size: f.size,
-                type: f.type
-            });
+
+            let match = pattern.exec(f.name);
+
+            if (match) {
+                this.files.push({
+                    name: f.name,
+                    path: f.path,
+                    size: f.size,
+                    type: f.type
+                });
+
+                updatePattern(this.initialPattern, 'raws', match[1]);
+                updatePattern(this.initialPattern, 'title', match[2]);
+                updatePattern(this.initialPattern, 'episode', episodePatternExtract(match[3], match[4]));
+                updatePattern(this.initialPattern, 'codec', match[6]);
+
+                console.log(match);
+            } else {
+                alert(`Cannot parse "${f.name}"`);
+            }
         }
         this.files.sort((f1, f2) => f1.name.localeCompare(f2.name));
 
-        console.log(this.files);
+        this.parsePattern();
 
         return false;
     }
 
     clearFiles() {
         this.files = [];
+
+        this.initialPattern = {
+            raws: PatternException.Empty,
+            title: PatternException.Empty,
+            episode: PatternException.Empty,
+            codec: PatternException.Empty,
+        };
+
+        this.parsePattern();
     }
 
     private parsePattern() {
+        if (this.initialPattern.raws == '') {
+            this.initialPattern.raws = PatternException.Empty;
+        }
+        if (this.initialPattern.title == '') {
+            this.initialPattern.title = PatternException.Empty;
+        }
+        if (this.initialPattern.episode == '') {
+            this.initialPattern.episode = PatternException.Empty;
+        }
+        if (this.initialPattern.codec == '') {
+            this.initialPattern.codec = PatternException.Empty;
+        }
+
         this.targetPattern = {
             raws: this.patternString(this.initialPattern.raws),
             title: this.patternString(this.initialPattern.title),
